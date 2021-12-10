fdc9236048c2b1d3c5b93dcc7df19cbc491052cf
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-12-10 13:22:18 +0100

    add section TourOurFacilities

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,6 +5,8 @@
   <MeetOurDoctors />
 
   <OurHealthServices />
+
+  <TourOurFacilities />
   
 </div>
   
@@ -14,12 +16,15 @@
 import WelcomeToAvadaHealth from '@/components/sectionMain/WelcomeToAvadaHealth.vue'
 import MeetOurDoctors from '@/components/sectionMain/MeetOurDoctors.vue'
 import OurHealthServices from '@/components/sectionMain/OurHealthServices.vue'
+import TourOurFacilities from '@/components/sectionMain/TourOurFacilities.vue'
+
 export default {
     name: 'Main',
     components: {
         WelcomeToAvadaHealth,
         MeetOurDoctors,
         OurHealthServices,
+        TourOurFacilities,
     }
 
 }
---
+++ src/components/sectionMain/TourOurFacilities.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="bg">
+ <div class="container ">
+    <div class="row">
+        <div class="text-center distance title_p  ">
+            <img src="../../assets/icon-7.png" alt="">
+           <h2 class="my-2">TOUR OUR FACILITIES</h2>
+           <div class="separator">_______________________________</div>
+           <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</p>
+           <p> doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo</p> 
+           <p>nventore veritatis et quasi architecto beatae</p> 
+           <img src="../../assets/play-icon.png" alt="">
+        </div>
+    </div>
+ </div>
+
+</section>
+</template>
+
+<script>
+export default {
+    name: 'TourOurFacilities',
+
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/globals";
+@import "@/styles/utilities";
+
+ .bg {
+     background-image:  url(../../assets/paralax-bg-tour-facilities.jpg);
+      height: 90vh;
+  background-size: cover;
+  background-position-y: -130px;
+ }
+ .title_p {
+     margin-top: 20%;
+     color: white;
+ }
+
+</style>
