62c56264ded4b21758027e14d5678ec0748eb2df
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-12-11 09:35:31 +0100

    add section MakeAnAppointment

--- src/components/sectionMain/MakeAnAppointment.vue
+++ src/components/sectionMain/MakeAnAppointment.vue
@@ -1,36 +1,40 @@
 <template>
   <section class="bg">
- <div class="container ">
-    <div class="row">
-        <div class="text-center distance title_p  ">
-           <h2 class="my-2">Make An Appointment</h2>
-           <div class="separator">_______________________________</div>
-           <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</p>
-           <p> doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo</p> 
-           <p>nventore veritatis et quasi architecto beatae</p> 
+    <div class="container">
+      <div class="row">
+        <div class="text-center distance title_p">
+          <h2 class="my-2">Make An Appointment</h2>
+          <div class="separator">_______________________________</div>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium
+          </p>
+          <p>
+            doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+          </p>
+          <p>nventore veritatis et quasi architecto beatae</p>
         </div>
         <div class="container">
-            <input type="text">
+          <div class="row">
+            <div class="d-flex boxes flex-wrap">
+              <input class="me-5 mt-5 px-5 py-2" type="text"  placeholder="Name*" />
+              <input class="mt-5 px-5" type="text" placeholder="Email*" />
+              <input class="me-5 mt-5 px-5 py-2" type="text" placeholder="Phone Number" />
+              <input class="mt-5 px-5" type="text" placeholder="Appointment Date" />
+              <textarea class="mt-5 px-5 py-2" name="" id="" cols="55" rows="4" placeholder="How can we help?*"></textarea>
+            </div>
+                <button class="btn_1 mt-5 py-3 mb-5">Make an appointment</button>
+          </div>
         </div>
-         <div class="container">
-            <input type="text">
-        </div>
-         <div class="container">
-            <input type="text">
-        </div>
-         <div class="container">
-            <input type="text">
-        </div>
+      </div>
     </div>
- </div>
-
-</section>
+  </section>
 </template>
 
 <script>
 export default {
- name: 'MakeAnAppointment',
-}
+  name: "MakeAnAppointment",
+};
 </script>
 
 <style scoped lang="scss">
@@ -38,14 +42,37 @@ export default {
 @import "@/styles/utilities";
 
 .bg {
-     background-image:  url(../../assets/make-an-appointment.jpg);
-    height: 800px;
+  background-image: url(../../assets/make-an-appointment.jpg);
+  height:1070px;
+  background-repeat: no-repeat;
   background-size: cover;
   background-position-y: -130px;
- }
- .title_p {
-   margin-top: 150px;
-     color: white;
- }
+ 
+}
+.title_p {
+  margin-top: 150px;
+  color: white;
+}
+ 
+.boxes {
+  width: 80%;
+margin-left: 23%;
+}
+input {
+  border: 2px solid $color-text-1;
+  background: none;
+  color: $color-text-1;
+}
+
+textarea {
+  border: 2px solid $color-text-1;
+  background: none;
+  color: $color-text-1;
+}
+button {
+  width: 702px;
+  margin-left: 315px;
+  
+}
 
 </style>
